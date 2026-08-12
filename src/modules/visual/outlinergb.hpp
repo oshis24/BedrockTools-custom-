1e9e80233bd0714e91924983515d0499d45eff86
Oshis24 <oshimatcha@gmail.com>
2026-08-12 20:01:45 +0700

    Update outlinergb.hpp

--- src/modules/visual/outlinergb.hpp
+++ src/modules/visual/outlinergb.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "../Module.hpp"
-
 #include <nlohmann/json.hpp>
 
 class OutlineRGBModule final : public Module {
@@ -13,18 +12,19 @@ public:
     void onEnable() override;
     void onDisable() override;
 
-    void loadConfig(
-        const nlohmann::json& j
-    ) override;
+    void loadConfig(const nlohmann::json& j) override;
+    void saveConfig(nlohmann::json& j) override;
 
-    void saveConfig(
-        nlohmann::json& j
-    ) override;
-
+    bool blockOutline = true;
+    bool entityOutline = true;
+    bool outline3D = true;
     bool rgbCycle = true;
 
     float red = 1.0f;
-    float green = 0.15f;
-    float blue = 0.95f;
+    float green = 0.0f;
+    float blue = 0.0f;
     float alpha = 1.0f;
+
+    float chromaSpeed = 1.0f;
+    float thickness = 1.0f;
 };
