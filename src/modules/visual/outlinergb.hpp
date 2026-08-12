983384cb8b62f8916f954fb82fbfd8b2d301dfb8
Oshis24 <oshimatcha@gmail.com>
2026-08-12 16:59:26 +0700

    Create outlinergb.hpp

---
+++ src/modules/visual/outlinergb.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../Module.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <cstdint>
+
+class OutlineRGBModule final : public Module {
+public:
+    OutlineRGBModule();
+    ~OutlineRGBModule() override;
+
+    void onInit() override;
+    void onEnable() override;
+    void onDisable() override;
+
+    void loadConfig(const nlohmann::json& j) override;
+    void saveConfig(nlohmann::json& j) override;
+
+    bool rgbCycle = true;
+
+    float red = 1.0f;
+    float green = 0.15f;
+    float blue = 0.95f;
+    float alpha = 1.0f;
+
+private:
+    void installHooks();
+};
