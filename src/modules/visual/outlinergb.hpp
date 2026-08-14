941cb5ec67a9888305f507380bc60e38610a584f
Oshis24 <oshimatcha@gmail.com>
2026-08-14 21:40:52 +0700

    Update outlinergb.hpp

--- src/modules/visual/outlinergb.hpp
+++ src/modules/visual/outlinergb.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "../Module.hpp"
+
 #include <nlohmann/json.hpp>
+
+#include <cstdint>
 
 class OutlineRGBModule final : public Module {
 public:
@@ -11,16 +14,37 @@ public:
     void onInit() override;
     void onEnable() override;
     void onDisable() override;
+    void onFrame() override;
 
     void loadConfig(const nlohmann::json& j) override;
     void saveConfig(nlohmann::json& j) override;
 
     bool rgbCycle = true;
-    bool outline3D = true;
 
-    float colorRed = 1.0f;
-    float colorGreen = 0.0f;
-    float colorBlue = 0.0f;
+    float red = 1.0f;
+    float green = 0.15f;
+    float blue = 0.95f;
+    float alpha = 1.0f;
 
-    float chromaSpeed = 1.0f;
+    float rgbSpeed = 0.25f;
+
+    /*
+     * 1 = single line
+     * 2 = 3 parallel lines
+     * 3 = 5 parallel lines
+     * ...
+     *
+     * This is a geometry-based thickness simulation because
+     * GLES line width is unreliable on Android/Mali.
+     */
+    int thickness = 1;
+
+private:
+    bool installHooks();
+
+    bool m_patched = false;
+    void* m_patchTarget = nullptr;
+
+public:
+    static OutlineRGBModule* instance;
 };
